7ea80c269d06dc798e4e475dbb7aec9e766c2e84
Emanuel Borges <ebbmango@tuta.io>
2022-11-12 16:52:31 -0300

    starts building posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,22 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+    # skip_authorization
   end
 
   def edit
@@ -18,5 +28,11 @@ class PostsController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :photo)
   end
 end
