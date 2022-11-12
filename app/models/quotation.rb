a0d188501202d3295ec653925bc1501648a0d9e6
Fernando Rodrigues <fnando@me.com>
2022-11-12 16:34:45 -0300

    quotations controller created

--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -1,3 +1,4 @@
 class Quotation < ApplicationRecord
   belongs_to :user
+  SIZES = ['Small', 'Medium', 'Large']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :addresses, only: %i[new create]
+  resources :quotations
   root to: "pages#home"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
