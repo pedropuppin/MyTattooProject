9e0592330362b74eb51ca19d908bcc425509d683
Emanuel Borges <ebbmango@tuta.io>
2022-11-12 15:31:39 -0300

    sets posts controller's routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :addresses, only: %i[new create]
-  root to: "pages#home"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "pages#home"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :addresses, only: %i[new create]
+  resources :posts
 end
