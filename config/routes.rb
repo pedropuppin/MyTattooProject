15733312261ec2c08ff81f1c2ca0922b35e1bb1e
Emanuel Borges <ebbmango@tuta.io>
2022-11-19 11:46:35 -0300

    solved problems derived from merging

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,5 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
-
-  resources :addresses, only: %i[new create]
   resources :posts
 end
