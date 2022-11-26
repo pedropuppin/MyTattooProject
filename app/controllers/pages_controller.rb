aac5da9f43727f5dbf60b115f7908a8e3d8daaf6
pedro puppin <puppin.pedro@gmail.com>
2022-11-26 15:46:04 -0300

    comments to post

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
+  skip_before_action :authenticate_user!, only: :home
   def home
     @user = current_user
     @posts = Post.group_em(policy_scope(Post).reverse, 4)
