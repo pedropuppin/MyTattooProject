2857c4285d909c3d05560c1990c25b65f9d55472
pedro puppin <puppin.pedro@gmail.com>
2022-11-30 18:32:57 -0300

    elementos de css e grid arrumados

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     @user = current_user
-    @posts = Post.group_em(policy_scope(Post).reverse, 4)
+    @posts = policy_scope(Post)
     @comment = Comment.new
     authorize @comment, policy_class: CommentPolicy
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,14 +4,14 @@ class Post < ApplicationRecord
   has_one_attached :photo
   has_many :likes, dependent: :destroy
 
-  def self.group_em(posts, ngroups)
-    n_per_group, left_over = posts.size.divmod(ngroups)
-    cum_off = 0
-    ngroups.times.map do |i|
-      n = n_per_group + (i < left_over ? 1 : 0)
-      a = posts[cum_off, n]
-      cum_off += n
-      a
-    end
-  end
+  # def self.group_em(posts, ngroups)
+  #   n_per_group, left_over = posts.size.divmod(ngroups)
+  #   cum_off = 0
+  #   ngroups.times.map do |i|
+  #     n = n_per_group + (i < left_over ? 1 : 0)
+  #     a = posts[cum_off, n]
+  #     cum_off += n
+  #     a
+  #   end
+  # end
 end
