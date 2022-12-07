75ab1b11573a80e98eb26b657f8c11dacae65710
Gabriel Gomes da Silva <gabrielgsilva16@gmail.com>
2022-12-06 21:26:12 -0300

    New post view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_03_144246) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_231847) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_03_144246) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "placement"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
